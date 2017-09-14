28944dc30511ddea96d1f43f42ced8c661652c01
vrkarthik14 <vrkarthik14@gmail.com>
2017-09-14 20:00:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,5 @@
-//bu
-//kjb
-var button=document.getElementById('counter');
-var counter = 0;
-function myfunction() {
-    counter=counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-    span.innerHTML="hello";
-    }
+ function myfunction(){
+               
+               document.getElementById("demo").innerHTML="hello buddy";
+               
+           }
