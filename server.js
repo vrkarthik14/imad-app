d328025a85c88bdd94a54bc92d73c868bca553f0
vrkarthik14 <vrkarthik14@gmail.com>
2017-09-14 18:59:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -29,6 +29,9 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/ui/main.js',function(req,res){
+   res.sendFile(path.join(_dirname,'ui','main.js')); 
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
