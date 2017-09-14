e72367eff8d044a4a00eb9f2ce4a079d12300c0f
vrkarthik14 <vrkarthik14@gmail.com>
2017-09-14 22:42:43 +0530

    Update reg.js

--- ui/reg.js
+++ ui/reg.js
@@ -1,39 +1,150 @@
-h1 {  
-margin-left: 70px;  
+function formValidation()  
+{  
+var uid = document.registration.userid;  
+var passid = document.registration.passid;  
+var uname = document.registration.username;  
+var uadd = document.registration.address;  
+var ucountry = document.registration.country;  
+var uzip = document.registration.zip;  
+var uemail = document.registration.email;  
+var umsex = document.registration.msex;  
+var ufsex = document.registration.fsex; if(userid_validation(uid,5,12))  
+{  
+if(passid_validation(passid,7,12))  
+{  
+if(allLetter(uname))  
+{  
+if(alphanumeric(uadd))  
+{   
+if(countryselect(ucountry))  
+{  
+if(allnumeric(uzip))  
+{  
+if(ValidateEmail(uemail))  
+{  
+if(validsex(umsex,ufsex))  
+{  
 }  
-form li {  
-list-style: none;  
-margin-bottom: 5px;  
+}   
 }  
+}   
+}  
+}  
+}  
+}  
+return false;  
   
-form ul li label{  
-float: left;  
-clear: left;  
-width: 100px;  
-text-align: right;  
-margin-right: 10px;  
-font-family:Verdana, Arial, Helvetica, sans-serif;  
-font-size:14px;  
+} function userid_validation(uid,mx,my)  
+{  
+var uid_len = uid.value.length;  
+if (uid_len == 0 || uid_len >= my || uid_len < mx)  
+{  
+alert("User Id should not be empty / length be between "+mx+" to "+my);  
+uid.focus();  
+return false;  
 }  
+return true;  
+}  
+function passid_validation(passid,mx,my)  
+{  
+var passid_len = passid.value.length;  
+if (passid_len == 0 ||passid_len >= my || passid_len < mx)  
+{  
+alert("Password should not be empty / length be between "+mx+" to "+my);  
+passid.focus();  
+return false;  
+}  
+return true;  
+}  
+function allLetter(uname)  
+{   
+var letters = /^[A-Za-z]+$/;  
+if(uname.value.match(letters))  
+{  
+return true;  
+}  
+else  
+{  
+alert('Username must have alphabet characters only');  
+uname.focus();  
+return false;  
+}  
+}  
+function alphanumeric(uadd)  
+{   
+var letters = /^[0-9a-zA-Z]+$/;  
+if(uadd.value.match(letters))  
+{  
+return true;  
+}  
+else  
+{  
+alert('User address must have alphanumeric characters only');  
+uadd.focus();  
+return false;  
+}  
+}  
+function countryselect(ucountry)  
+{  
+if(ucountry.value == "Default")  
+{  
+alert('Select your country from the list');  
+ucountry.focus();  
+return false;  
+}  
+else  
+{  
+return true;  
+}  
+}  
+function allnumeric(uzip)  
+{   
+var numbers = /^[0-9]+$/;  
+if(uzip.value.match(numbers))  
+{  
+return true;  
+}  
+else  
+{  
+alert('ZIP code must have numeric characters only');  
+uzip.focus();  
+return false;  
+}  
+}  
+function ValidateEmail(uemail)  
+{  
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;  
+if(uemail.value.match(mailformat))  
+{  
+return true;  
+}  
+else  
+{  
+alert("You have entered an invalid email address!");  
+uemail.focus();  
+return false;  
+}  
+} function validsex(umsex,ufsex)  
+{  
+x=0;  
   
-form ul li input, select, span {  
-float: left;  
-margin-bottom: 10px;  
+if(umsex.checked)   
+{  
+x++;  
+} if(ufsex.checked)  
+{  
+x++;   
 }  
-  
-form textarea {  
-float: left;  
-width: 350px;  
-height: 150px;  
+if(x==0)  
+{  
+alert('Select Male/Female');  
+umsex.focus();  
+return false;  
 }  
-  
-[type="submit"] {  
-clear: left;  
-margin: 20px 0 0 230px;  
-font-size:18px  
+else  
+{  
+alert('Form Succesfully Submitted');  
+window.location.reload()  
+return true;  
 }  
-  
-p {  
-margin-left: 70px;  
-font-weight: bold;  
-}  
+}
