b8ea2d854adab1df986704c7a2588a90a706735c
vrkarthik14 <vrkarthik14@gmail.com>
2017-09-14 16:30:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,9 @@
-var name_list=new Array("Al","Betty","Kasper","MicheL","Robert","Zimbo");
-var new_name,index,last;
-while(new_name==prompt("Please type a new name"," ")){
-	last=name_list.length-1;
-	while(last>=0&&name_list[last] > new_name){
-		name_list[last+1]=name_list[last];
-		last--;
-	}
-	name_list[last+1]=new_name;
-	document.write("<p><strong>The new name list is :</strong>","</br>");
-	for(index=0;index<name_list.length;index++)
-		document.write(name_list[index],"</br>");
-	document.write("<p>");
-}
+var button=document.getElementById('counter');
+var counter=0;
+
+button.onclick= function() {
+    counter+=1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+};
